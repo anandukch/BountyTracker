618ef78b2af54a41737fe7057ae549d6c263e389
Anandu <anandukch@gmail.com>
2024-07-19 16:00:18 +0530

    chore: added additional fields in employee table

--- backend/src/entity/employee.entity.ts
+++ backend/src/entity/employee.entity.ts
@@ -1,16 +1,25 @@
 import { Column, Entity, ManyToOne, OneToMany, OneToOne } from "typeorm";
 import AbstractEntity from "./abstract.entity";
+import { Role } from "../utils/role.enum";
 
 @Entity()
 class Employee extends AbstractEntity {
     @Column()
     name: string;
 
-    @Column()
+    @Column({
+        unique: true,
+    })
     email: string;
 
     @Column()
     age: number;
+
+    @Column()
+    role: Role;
+
+    @Column()
+    totalBounty: number;
 }
 
 export default Employee;
