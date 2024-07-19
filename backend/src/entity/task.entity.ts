a89bcd6ff9bcf5314faa0bbe12f02518be4d5912
Anandu <anandukch@gmail.com>
2024-07-19 16:29:27 +0530

    add: added basic task entity

--- backend/src/entity/task.entity.ts
+++ backend/src/entity/task.entity.ts
@@ -1,9 +1,20 @@
 import { Column, Entity, ManyToOne, OneToMany, OneToOne } from "typeorm";
 import AbstractEntity from "./abstract.entity";
+import Employee from "./employee.entity";
 
 @Entity()
 class Task extends AbstractEntity {
-    // TODO
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: string;
+
+    @ManyToOne(() => Employee, (employee) => employee.tasks)
+    createBy: number;
 }
 
 export default Task;
