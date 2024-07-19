44db2abccd9a8b1127fc9ed1185ce0dfa6102b3b
technodumb <alnaskabeer@gmail.com>
2024-07-19 15:34:41 +0530

    chore: change typo

--- backend/src/utils/requestWithRole.ts
+++ backend/src/utils/requestWithRole.ts
@@ -1,7 +1,7 @@
 import { Request } from "express";
 import { Role } from "./role.enum";
 
-export interface RequestWithUser extends Request {
+export interface RequestWithRole extends Request {
     name: string;
     email: string;
     role: Role;
