660c78fff3c1107335cbf01d971e1ad5f77c8a8f
Nalin Govind V <nalingovind@gmail.com>
2024-07-20 15:39:56 +0530

    Employee dashboard created

---
+++ frontend/src/components/DetailBlock.jsx
@@ -0,0 +1,9 @@
+const DetailBlock = () => {
+  return (
+    <div className="employeeDetailBlock">
+      <h4>Gender</h4>
+      <p>Female</p>
+    </div>
+  );
+};
+export default DetailBlock;
---
+++ frontend/src/components/TaskRowData.jsx
@@ -0,0 +1,16 @@
+import TaskColumnData from "./TaskColumnData";
+
+const TaskDataRow = () => {
+  return (
+    <div className="taskDataRow">
+      <TaskColumnData />
+      <TaskColumnData />
+      <TaskColumnData />
+      <TaskColumnData />
+      <TaskColumnData />
+      <TaskColumnData />
+    </div>
+  );
+};
+
+export default TaskDataRow;
