86df606dc83a1ae17f186ee2c66e25a7f2b3dde9
Nalin Govind V <nalingovind@gmail.com>
2024-07-22 16:27:33 +0530

    conflict resolved in employee dashboard

--- frontend/src/components/TaskRowData.jsx
+++ frontend/src/components/TaskRowData.jsx
@@ -2,21 +2,23 @@ import { formatDate } from "../utils/date.utils";
 import TaskColumnData from "./TaskColumnData";
 
 const TaskDataRow = ({ taskRows = [] }) => {
-  return (
-    <div className="taskDataRow">
-      <TaskColumnData content={taskRows.task.title} />
-      <TaskColumnData content={taskRows.task.createdBy.name} />
-      <TaskColumnData
-        content={formatDate(taskRows.task.deadLine)}
-      />
-      <TaskColumnData
-        content={`
-          ${taskRows.task.currentParticipants} / ${taskRows.task.maxParticipants}`}
-      />
-      <TaskColumnData content={taskRows.task.status} />
-      <TaskColumnData content={taskRows.contribution} />
-    </div>
-  );
+	let status = "In Progress";
+	if (taskRows.task.status == "completed") status = "Completed";
+	return (
+		<div className="taskDataRow">
+			<TaskColumnData content={taskRows.task.title || ""} />
+			<TaskColumnData content={taskRows.task.createdBy.name || ""} />
+			<TaskColumnData content={formatDate(taskRows.task.deadLine) || ""} />
+			<TaskColumnData
+				content={
+					`
+          ${taskRows.task.currentParticipants} / ${taskRows.task.maxParticipants}` || ""
+				}
+			/>
+			<TaskColumnData content={status || ""} />
+			<TaskColumnData content={taskRows.contribution || 0} />
+		</div>
+	);
 };
 
 export default TaskDataRow;
