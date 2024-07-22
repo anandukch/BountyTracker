4047d39a3873d028c476dbdbd90abb447d308a6f
Anandu <anandukch@gmail.com>
2024-07-22 12:42:16 +0530

    refactor: task page

--- frontend/src/pages/CreateTask/createTask.jsx
+++ frontend/src/pages/CreateTask/createTask.jsx
@@ -3,64 +3,86 @@ import Button from "../../components/Button/Button";
 import FormComponent from "../../components/FormComponent/FormComponent";
 import "./styles.scss";
 import Select from "../../components/Select/Select";
+
+const initialFormData = {
+	title: "",
+	description: "",
+	dueDate: "",
+	startDate: "",
+	skills: "",
+	bounty: "",
+	maxParticipants: "",
+};
 const CreateTask = () => {
-   const [formData, setFormData] = useState({});
-   const form_fields=[
-      {
-         id: "name",
-         name: "Task Name",
-         type: "text",
-      },
-      {
-         id: "bounty",
-         name: "Bounty",
-         type: "number",
-      },
-      {
-         id: "description",
-         name: "Description",
-         type: "text",
-         Component: "text-area",
-      },
-      {
-         id: "due date",
-         name: "Due Date",
-         type: "date",
-      },
-      {
-         id: "type",
-         name: "Type",
-         values: [{ option: "Individual" }, { option: "Group" }],
-         Component: Select,
-      },
-      {
-         id: "skills",
-         name: "Skills",
-         type: "text",
-      },
-   ];
-   const handleChange = (props) => {
-      setFormData((prevState) => {
-         if (formData.type === "Individual") delete formData.maxParticipants;
-         return { ...prevState, ...props };
-      });
-   };
-   useEffect(() => {
+	const [formData, setFormData] = useState(initialFormData);
+	const formFields = [
+		{
+			id: "name",
+			label: "Task Name",
+         name: "title",
+			type: "text",
+		},
+		{
+			id: "bounty",
+			label: "Bounty",
+         name: "bounty",
+			type: "number",
+		},
+		{
+			id: "description",
+			label: "Description",
+			type: "text",
+         name: "description",
+			Component: "text-area",
+		},
+		{
+			id: "due date",
+			label: "Due Date",
+			type: "date",
+         name:"deadLine"
+		},
+		// {
+		//    id: "type",
+		//    label: "Type",
+		//    values: [{ option: "Individual" }, { option: "Group" }],
+		//    Component: Select,
+		// },
+		{
+			id: "Start Date",
+			label: "Start Date",
+			type: "date",
+         name: "startDate",
+		},
+		{
+			id: "skills",
+			label: "Skills",
+			type: "text",
+         name: "skills",
+		},
+	];
+	const handleChange = (e) => {
+		setFormData((prev) => ({
+         ...prev,
+         [e.target.name]: e.target.value,
+     }));
+	};
+	const createTaskHandler = () => {
       console.log(formData);
-   }, [formData]);
-   return (
-      <main className="createTask">
-         <div className="title">
-            <span>Add Task </span>
-            <div className="formButtons">
-               <Button text="Create" className="create" />
-               <Button text="Cancel" className="cancel" />
-               {/* <Link to="/employees" className="cancelLink"> */}
-               {/* </Link> */}
-            </div>
-         </div>
-         <FormComponent formFields = {form_fields} onChange={handleChange} />
-      </main>
-   );
+      console.log("Task Created");
+   }
+	return (
+		<main className="createTask">
+			<div className="title">
+				<span>Add Task </span>
+				<div className="formButtons">
+					<Button text="Create" className="create" onClick={createTaskHandler}/>
+					<Button text="Cancel" className="cancel" />
+					{/* <Link to="/employees" className="cancelLink"> */}
+					{/* </Link> */}
+				</div>
+			</div>
+			<FormComponent formFields={formFields} onChange={handleChange} />
+		</main>
+	);
 };
 export default CreateTask;
