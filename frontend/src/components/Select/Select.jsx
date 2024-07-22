a5ad0cb1ad59b74b045a023c8bce21dcf81168f1
Anandu <anandukch@gmail.com>
2024-07-22 15:37:20 +0530

    add:added state mamagement and form fix in register employee

--- frontend/src/components/Select/Select.jsx
+++ frontend/src/components/Select/Select.jsx
@@ -1,27 +1,25 @@
 import { useState } from "react";
 
-const Select = ({values,label,className,onChange}) => {
-   //  const handleSelect = (e) => {
-   //      if (onSelect) onSelect(e.target.value, label);
-   //   };
-   return (
-      <span className={className}>
-         <label htmlFor={label}> {label} </label>
-         <select defaultValue ="default" onChange={onChange}>
-            <option value="default" disabled>
-               {label}
-            </option>
-            {values.map((element) => {
-               return (
-                  <option key={element.option} value={element.option} >
-                     {element.option}
-                  </option>
-               );
-            })}
-         </select>
-      </span>
-   );
+const Select = ({ values, label, name, className, onChange }) => {
+	//  const handleSelect = (e) => {
+	//      if (onSelect) onSelect(e.target.value, label);
+	//   };
+	return (
+		<span className={className}>
+			<label htmlFor={label}> {label} </label>
+			<select defaultValue="default" onChange={onChange} name={name}>
+				<option value="default" disabled>
+					{label}
+				</option>
+				{values.map((element) => {
+					return (
+						<option key={element.option} value={element.option}>
+							{element.option}
+						</option>
+					);
+				})}
+			</select>
+		</span>
+	);
 };
 export default Select;
-
-
