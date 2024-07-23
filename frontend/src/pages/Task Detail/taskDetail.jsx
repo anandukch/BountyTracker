f634efe5aead1c82e8bfc9f252c00051b104feac
technodumb <alnaskabeer@gmail.com>
2024-07-23 17:16:32 +0530

    fix: scroll particiapant list

--- frontend/src/pages/Task Detail/taskDetail.jsx
+++ frontend/src/pages/Task Detail/taskDetail.jsx
@@ -71,6 +71,7 @@ const TaskDetail = () => {
 		formData.append("commentType", commentType);
 		formData.append("content", comment);
 		createComment({ taskId, formData });
+		setComment("");
 	};
 	const handleTextArea = (e) => {
 		setComment(e.target.value);
