699adf153bd224f51cc06484be5f657dbb4dd4de
Anandu <anandukch@gmail.com>
2024-07-23 15:19:05 +0530

    refactor: refactored repositories

--- backend/src/repository/comment.repository.ts
+++ backend/src/repository/comment.repository.ts
@@ -1,41 +1,50 @@
 import { Repository } from "typeorm";
 import Comment from "../entity/comment.entity";
+import BaseRepository from "./base.repository";
 
-class CommentRepository {
-	constructor(private repository: Repository<Comment>) {}
+// class CommentRepository {
+// 	constructor(private repository: Repository<Comment>) {}
 
-	find = async (filter?: Partial<Comment>, relations?: Array<string>): Promise<Comment[]> => {
-		return this.repository.find({
-			where: filter,
-			relations: relations,
-		});
-	};
+// 	find = async (filter?: Partial<Comment>, relations?: Array<string>): Promise<Comment[]> => {
+// 		return this.repository.find({
+// 			where: filter,
+// 			relations: relations,
+// 		});
+// 	};
 
-	findOneBy = async (filter: Partial<Comment>, relations?: Array<string>): Promise<Comment> => {
-		return this.repository.findOne({
-			where: filter,
-			relations,
-		});
-	};
+// 	findOneBy = async (filter: Partial<Comment>, relations?: Array<string>): Promise<Comment> => {
+// 		return this.repository.findOne({
+// 			where: filter,
+// 			relations,
+// 		});
+// 	};
 
-	create = async (data: Comment): Promise<Comment> => {
-		return this.repository.create(data);
-	};
-	update = async (id: number, comment: Partial<Comment>) => {
-		return this.repository.update({ id }, comment);
-	};
+// 	create = async (data: Comment): Promise<Comment> => {
+// 		return this.repository.create(data);
+// 	};
+// 	update = async (id: number, comment: Partial<Comment>) => {
+// 		return this.repository.update({ id }, comment);
+// 	};
 
-	save = async (comment: Comment): Promise<Comment> => {
-		return this.repository.save(comment);
-	};
+// 	save = async (comment: Comment): Promise<Comment> => {
+// 		return this.repository.save(comment);
+// 	};
 
-	delete = async (filter: Partial<Comment>): Promise<void> => {
-		await this.repository.delete(filter.id);
-	};
+// 	delete = async (filter: Partial<Comment>): Promise<void> => {
+// 		await this.repository.delete(filter.id);
+// 	};
 
-	softDelete = async (comment: Comment): Promise<void> => {
-		await this.repository.softRemove(comment);
-	};
+// 	softDelete = async (comment: Comment): Promise<void> => {
+// 		await this.repository.softRemove(comment);
+// 	};
+// }
+
+// export default CommentRepository;
+
+class CommentRepository extends BaseRepository<Comment> {
+	constructor(repository: Repository<Comment>) {
+		super(repository);
+	}
 }
 
 export default CommentRepository;
--- backend/src/repository/taskParticipant.repository.ts
+++ backend/src/repository/taskParticipant.repository.ts
@@ -1,45 +1,6 @@
-// import { Repository } from "typeorm";
-// import TaskParticipants from "../entity/taskParticipants.entity";
-
 import { Repository } from "typeorm";
 import TaskParticipants from "../entity/taskParticipants.entity";
 import BaseRepository from "./base.repository";
-
-// class TaskParticipantRepository {
-// 	constructor(private repository: Repository<TaskParticipants>) {}
-
-// 	find = async (filter?: Partial<TaskParticipants>, relations?: Array<string>): Promise<TaskParticipants[]> => {
-// 		return this.repository.find({
-// 			where: filter,
-// 			relations: relations,
-// 		});
-// 	};
-
-// 	findOneBy = async (filter?: Partial<TaskParticipants>, relations?: Array<string>): Promise<TaskParticipants> => {
-// 		return this.repository.findOne({ where: filter, relations: relations });
-// 	};
-
-// 	create = async (data: TaskParticipants): Promise<TaskParticipants> => {
-// 		return this.repository.create(data);
-// 	};
-// 	update = async (id: number, task: Partial<TaskParticipants>) => {
-// 		return this.repository.update({ id }, task);
-// 	};
-
-// 	save = async (task: TaskParticipants): Promise<TaskParticipants> => {
-// 		return this.repository.save(task);
-// 	};
-
-// 	delete = async (filter: Partial<TaskParticipants>): Promise<void> => {
-// 		await this.repository.delete(filter.id);
-// 	};
-
-// 	softDelete = async (task: TaskParticipants): Promise<void> => {
-// 		await this.repository.softRemove(task);
-// 	};
-// }
-
-// export default TaskParticipantRepository;
 
 class TaskParticipantRepository extends BaseRepository<TaskParticipants> {
 	constructor(repository: Repository<TaskParticipants>) {
