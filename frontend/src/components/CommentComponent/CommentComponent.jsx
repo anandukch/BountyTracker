b97a5f6939c76c4fbb0460001dffff4ecb658738
sanojrc <sanojrc33@gmail.com>
2024-07-24 09:41:32 +0530

    Fix:  refactor comment

--- frontend/src/components/CommentComponent/CommentComponent.jsx
+++ frontend/src/components/CommentComponent/CommentComponent.jsx
@@ -4,15 +4,6 @@ const CommentComponent1 = ({ comment, handleReplyClick, currentEmployeeId }) => 
 	const isOwnComment = comment.employee.id === currentEmployeeId;
 	// console.log(comment);
 	return (
-		<div className="commentRecord" style={flagType ? (flagUser ? { justifyContent: "right" } : null) : null}>
-			{!flagUser ? <div className="name">{name}</div> : null}
-			<div className="comment" style={flagType ? (flagUser ? styleCommentUser : styleCommentNotUser) : null}>
-				{comment}
-				{!flagType ? (
-					<div className="reviewStatus" onClick={onClick}>
-						{status}
-					</div>
-				) : null}
 		<>
 			<div className={`commentWrapper ${isOwnComment ? "ownComment" : ""}`}>
 				<div className="commentBody">
