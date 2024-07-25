fa8dcdeb175b2ac4e4dfb2cc6918c211da34901c
Anandu <anandukch@gmail.com>
2024-07-25 12:07:03 +0530

    feat:redeem request feature added

--- backend/src/entity/RedeemRequest.ts
+++ backend/src/entity/RedeemRequest.ts
@@ -1,9 +1,20 @@
-import { Column, Entity } from "typeorm";
+import { Column, Entity, ManyToOne } from "typeorm";
 import AbstractEntity from "./abstract.entity";
+import Employee from "./employee.entity";
 
 @Entity()
 class RedeemRequest extends AbstractEntity {
-		
+	@ManyToOne(() => Employee, (employee) => employee.comments)
+	employee: Employee;
+
+	@Column()
+	content: string;
+
+	@Column()
+	status: string;
+
+	@Column()
+	redeemAmount: number;
 }
 
 export default RedeemRequest;
