539f88a906951914a3b5447516df49897ebfdc82
technodumb <alnaskabeer@gmail.com>
2024-07-25 11:43:33 +0530

    add: download attachemtn from comment

--- frontend/src/components/CommentComponent/CommentComponent.jsx
+++ frontend/src/components/CommentComponent/CommentComponent.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import "./styles.scss";
+import download from "../../assets/download.svg";
 
 const CommentComponent = ({ comment, handleReplyClick, currentEmployeeEmail }) => {
 	const isOwnComment = comment?.employee?.email === currentEmployeeEmail;
@@ -7,15 +8,23 @@ const CommentComponent = ({ comment, handleReplyClick, currentEmployeeEmail }) =
 		comment && (
 			<>
 				<div className={`commentWrapper ${isOwnComment ? "ownComment" : ""}`}>
-					<div className="commentBody">
-						{!isOwnComment && <span className="commentUsername">{comment.employee.name}</span>}
-						{comment.mentionComment && (
-							<span className="mentionComment">
-								<span className="mentionedEmployee">{comment.mentionComment.employee.name}</span>
-								<span className="mentionedCommentContent">{comment.mentionComment.content}</span>
-							</span>
+					<div className="commentBodyWithAttachment">
+						<div className="commentBody">
+							{!isOwnComment && <span className="commentUsername">{comment.employee.name}</span>}
+							{comment.mentionComment && (
+								<span className="mentionComment">
+									<span className="mentionedEmployee">{comment.mentionComment.employee.name}</span>
+									<span className="mentionedCommentContent">{comment.mentionComment.content}</span>
+								</span>
+							)}
+							<span className="commentContent">{comment.content}</span>
+						</div>
+						{comment.fileUrl && (
+							<button className="downloadButton" onClick={() => comment.fileUrl}>
+								<img src={download}></img>
+							</button>
 						)}
-						<span className="commentContent">{comment.content}</span>
+						{/* TODO: button link */}
 					</div>
 					<span className="replyLink" onClick={() => handleReplyClick(comment.id)}>
 						Reply
